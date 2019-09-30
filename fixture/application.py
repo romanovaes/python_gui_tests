221bf2eec158d32d862ba8a7e4e89c2742efb246
Romanova <romanove.e.s@effective-group.ru>
2019-09-30 20:18:54 +0300

    тест добавления группы

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,15 @@
-class Application:
-     def __index__(self, target):
-         pass
+from pywinauto.application import Application as WinApplication
 
-     def destroy
+from fixture.group import GroupHelper
+
+
+class Application:
+
+    def __init__(self, target):
+        self.application = WinApplication(backend="win32").start(target)
+        self.main_window=self.application.window(title="Free Address Book")
+        self.main_window.wait("visible")
+        self.groups=GroupHelper(self)
+
+    def destroy(self):
+       self.main_window.close()
